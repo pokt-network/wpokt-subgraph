3be2b7b0db0150abf3695db956fc5c22a195129c
CrisOG <40803711+crisog@users.noreply.github.com>
2021-02-15 15:25:14 -0400

    Implement handleStaked mappings (#10)

---
+++ src/util/constants.ts
@@ -0,0 +1,9 @@
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts';
+
+// BigDecimal
+export let ONE_BIG_DECIMAL = BigDecimal.fromString('1');
+export let ZERO_BIG_DECIMAL = BigDecimal.fromString('0');
+
+// BigInt
+export let ONE_BIG_INT = BigInt.fromI32(1);
+export let ZERO_BIG_INT = BigInt.fromI32(0);
