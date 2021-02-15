21473ef5a54f5628f79acf914c8ff6674e02b42a
Cristopher Ortega <ortega.cpp@gmail.com>
2021-02-15 13:57:21 -0400

    Configure subgraph + add rinkeby contract addresses

---
+++ src/mappings/tokenGeyser.ts
@@ -0,0 +1,17 @@
+import {
+  Staked,
+  Unstaked,
+  TokensClaimed,
+  TokensLocked,
+  TokensUnlocked
+} from '../types/TokenGeyser/TokenGeyser';
+
+export function handleStaked(event: Staked): void {}
+
+export function handleUnstaked(event: Unstaked): void {}
+
+export function handleTokensClaimed(event: TokensClaimed): void {}
+
+export function handleTokensLocked(event: TokensLocked): void {}
+
+export function handleTokensUnlocked(event: TokensUnlocked): void {}
